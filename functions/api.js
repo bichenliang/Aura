f0256b71819ad85c8546d1e08353a114ebac7459
bichenliang <1205638884@qq.com>
2026-08-19 17:07:34 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,8 @@
 export async function onRequestGet(context) {
-  return new Response(JSON.stringify({status:"ok","msg":"hello test"}),{
+  return new Response(JSON.stringify({
+    envKeys: Object.keys(context.env),
+    hasD1: !!context.env.D1_DB
+  }),{
     headers:{"Content-Type":"application/json"}
   })
 }
