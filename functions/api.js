2de95d72111cdb5d65759dabeed9e52a0f57e3cd
bichenliang <1205638884@qq.com>
2026-08-19 16:54:06 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,5 @@
 export async function onRequestGet(context) {
-  const db = context.env.D1_DB;
-  try {
-    const res = await db.prepare("SELECT * FROM products").all();
-    return new Response(JSON.stringify(res.results), {
-      headers: { "Content-Type": "application/json" }
-    });
-  } catch (err) {
-    return new Response(JSON.stringify({
-      errMsg: err.message
-    }), {
-      status:500,
-      headers: { "Content-Type": "application/json" }
-    });
-  }
+  return new Response(JSON.stringify({status:"ok","msg":"hello test"}),{
+    headers:{"Content-Type":"application/json"}
+  })
 }
